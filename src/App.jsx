a58b106cb9220d11e3badbb1180d903155b317c8
Ignacio García-Barrero <igarciabarrero@outlook.com>
2022-11-16 17:26:58 +0100

    bubble and curve updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more yeay!
-      </p>
+
+ 
+
+      <section class="bubble">
+        <h1>Nice Curves</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero deserunt praesentium eum quia, nesciunt minima, labore accusamus dolores at ea ex nostrum consectetur aut minus et debitis eligendi laudantium ratione.</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Soluta pariatur ipsum non placeat error veritatis voluptas accusantium tempore in, nobis, assumenda similique nostrum sit laboriosam aut eaque nulla alias nisi! Enim alias molestiae est iste impedit facere ab.</p>
+        <div class="curve"></div>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Ex excepturi vitae accusamus blanditiis nesciunt, est necessitatibus saepe dolore soluta cum molestiae! Ipsa neque exercitationem, aliquid accusantium eos expedita error velit ea accusamus quam voluptatum omnis nisi pariatur veritatis!</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Nam dicta vel ducimus? Dolor fuga labore deleniti aspernatur debitis est autem impedit atque sapiente sequi corrupti, possimus nesciunt amet exercitationem sunt commodi a? Odit iure illum rerum impedit? Nihil!</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Sint neque fugit consequatur amet provident minima voluptate repudiandae ducimus, aliquid tenetur aspernatur nobis quibusdam possimus laborum deserunt, iure perferendis quisquam accusamus quam incidunt culpa id dolor. Id, ipsum dolore.</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Omnis, laboriosam at rem delectus aperiam veritatis. Exercitationem esse numquam reiciendis voluptate nostrum sed officiis sapiente porro nihil, modi, commodi placeat quisquam quasi neque saepe odio, laboriosam aliquam corporis pariatur?</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Officiis voluptate laudantium facere autem vero provident maxime amet beatae voluptatum! Quidem, fugit a, architecto placeat, maiores sit iusto quia doloribus officiis tempora praesentium accusantium perferendis labore quod distinctio quasi.</p>
+      </section>
+      <section>
+        <h1>Nice Curves</h1>
+        <p>Distinctio eos, asperiores cum, neque explicabo fuga voluptatem veniam incidunt officiis quasi sapiente assumenda dolores est odit eaque aliquid odio in. Debitis vero reprehenderit ipsam eos a nostrum quo beatae.</p>
+      </section>
     </div>
   )
 }
