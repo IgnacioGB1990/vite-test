ffc1becca3799b23d29e4e7489c8b338d621874e
Ignacio García-Barrero <igarciabarrero@outlook.com>
2022-11-17 20:07:19 +0100

    Added folders and curves

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
 
@@ -8,40 +9,7 @@ function App() {
     <div className="App">
 
  
-
-      <section class="bubble">
-        <h1>Esas curvas</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero deserunt praesentium eum quia, nesciunt minima, labore accusamus dolores at ea ex nostrum consectetur aut minus et debitis eligendi laudantium ratione.</p>
-      </section>
-      <section>
-        <h1>Nice Curves</h1>
-        <p>Soluta pariatur ipsum non placeat error veritatis voluptas accusantium tempore in, nobis, assumenda similique nostrum sit laboriosam aut eaque nulla alias nisi! Enim alias molestiae est iste impedit facere ab.</p>
-        <div class="curve"></div>
-      </section>
-      <section>
-        <h1>Nice Curves</h1>
-        <p>Ex excepturi vitae accusamus blanditiis nesciunt, est necessitatibus saepe dolore soluta cum molestiae! Ipsa neque exercitationem, aliquid accusantium eos expedita error velit ea accusamus quam voluptatum omnis nisi pariatur veritatis!</p>
-      </section>
-      <section>
-        <h1>Nice Curves</h1>
-        <p>Nam dicta vel ducimus? Dolor fuga labore deleniti aspernatur debitis est autem impedit atque sapiente sequi corrupti, possimus nesciunt amet exercitationem sunt commodi a? Odit iure illum rerum impedit? Nihil!</p>
-      </section>
-      <section>
-        <h1>Nice Curves</h1>
-        <p>Sint neque fugit consequatur amet provident minima voluptate repudiandae ducimus, aliquid tenetur aspernatur nobis quibusdam possimus laborum deserunt, iure perferendis quisquam accusamus quam incidunt culpa id dolor. Id, ipsum dolore.</p>
-      </section>
-      <section>
-        <h1>Nice Curves</h1>
-        <p>Omnis, laboriosam at rem delectus aperiam veritatis. Exercitationem esse numquam reiciendis voluptate nostrum sed officiis sapiente porro nihil, modi, commodi placeat quisquam quasi neque saepe odio, laboriosam aliquam corporis pariatur?</p>
-      </section>
-      <section>
-        <h1>Nice Curves</h1>
-        <p>Officiis voluptate laudantium facere autem vero provident maxime amet beatae voluptatum! Quidem, fugit a, architecto placeat, maiores sit iusto quia doloribus officiis tempora praesentium accusantium perferendis labore quod distinctio quasi.</p>
-      </section>
-      <section>
-        <h1>Nice Curves</h1>
-        <p>Distinctio eos, asperiores cum, neque explicabo fuga voluptatem veniam incidunt officiis quasi sapiente assumenda dolores est odit eaque aliquid odio in. Debitis vero reprehenderit ipsam eos a nostrum quo beatae.</p>
-      </section>
+<HomeScreen/>
     </div>
   )
 }
