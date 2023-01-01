75b7d0004fc7adf2969d9c5b2da837efaacfac17
Ignacio García-Barrero <igarciabarrero@outlook.com>
2023-01-01 13:27:20 +0100

    First 2023 commit

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -20,15 +20,19 @@ const HomeScreen = () => {
       <section class="bubble">
         <ul>
           <li>Intercambia un libro con alguien cercano</li>
-          <li>Ofrece a alguien algún libro que tengas por casa y libérate de espacio</li>
-          <li>Aprovecháte de gente que ya no use aquel libro que necesitas</li>
+          <li>Aprovecháte de gente que dona libros que ya no usa</li>
+          <li>¿Necesitas espacio en casa? Dónalos a gente que les pueda sacar más provecho.</li>
       </ul>
 
     </section>
+    
 
-
-    <section class="bubble">
+    <section class="search">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+        </svg>
        <p>Introduce tu dirección y encuentra libros cerca de ti</p>
+   
 
     </section>
   
